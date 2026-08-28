84a0030edc0ca64c28d989771d77e8e2515866f7
Simo Tuomisto <simo.tuomisto@aalto.fi>
2026-08-28 10:06:46 +0300

    Added new env template list-command

---
+++ cmd/env/template/list.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"go.yaml.in/yaml/v3"
+)
+
+var listTemplateCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all shark-tank environment templates",
+	Long: `List all shark-tank environment templates.
+
+This command allows you to view all your current shark-tank environment templates in a structured format.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		viperTemplates := viper.Sub("envtemplates")
+		if viperTemplates != nil {
+			c := viperTemplates.AllSettings()
+			bs, err := yaml.Marshal(c)
+			if err != nil {
+				log.Error().Err(err).Msg("Error marshalling environment templates")
+				return
+			}
+			fmt.Println(string(bs))
+		} else {
+			fmt.Println("No environment templates configured.")
+		}
+	},
+}
+
+func init() {
+	templateCmd.AddCommand(listTemplateCmd)
+}
